448eeee08de3819dadf93e4ae3c9d1e96808ce4d
Himanshu2557 <96951932+Himanshu2557@users.noreply.github.com>
2022-01-07 12:11:09 +0530

    Add files via upload

---
+++ Assignment_1 output.py
@@ -0,0 +1,35 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: /Users/himanshugoyal/Documents/College/ASSIGNMENT/computer/Assignment_1/q1.py
+         Question-1 
+Enter First Number
+2
+Enter Second Number
+3
+Enter Third Number
+4
+Average of numbers:  3.0
+
+= RESTART: /Users/himanshugoyal/Documents/College/ASSIGNMENT/computer/Assignment_1/q2.py
+         Question-2 
+Enter your gross income: 50000
+Enter Number of dependents: 4
+Your total tax: 5600.0 $
+
+= RESTART: /Users/himanshugoyal/Documents/College/ASSIGNMENT/computer/Assignment_1/Q3.py
+         Question-3 
+[SID, Name, Gender, Course Name, CGPA]
+[21105029, 'Himanshu', 'M', 'Intro to Computing', 9.7]
+
+= RESTART: /Users/himanshugoyal/Documents/College/ASSIGNMENT/computer/Assignment_1/q4.py
+         Question-4 
+Enter Marks Of Students: 43 43 53 45 3 467 4 6 5
+[3, 4, 5, 6, 43, 43, 45, 53, 467]
+
+= RESTART: /Users/himanshugoyal/Documents/College/ASSIGNMENT/computer/Assignment_1/q5.py
+         Question-5 
+Part A
+['Red', 'Green', 'White', 'Black', 'Yellow']
+Part B
+['Red', 'Green', 'White', 'Purple', 'Yellow']
