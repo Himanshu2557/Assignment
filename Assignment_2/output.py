08200a4c22f425e04d033f1bd384d93f466ed08c
Himanshu2557 <96951932+Himanshu2557@users.noreply.github.com>
2022-01-17 18:22:57 +0530

    Update output.py

--- Assignment_2/output.py
+++ Assignment_2/output.py
@@ -1,1 +1,52 @@
+*************** Solution 1 ******************
+a.
+Lenght of the string is: 36
+b.
+The reversed string would be: egauganal evitisnes esac a si nohtyP
+c.
+New string: case sensitive
+d.
+Replaced string is: Python is a object oriented lanaguage
+e.
+Index of first occurrence of substring "a" is: 10
+f.
+String without white spaces is: Pythonisacasesensitivelanaguage
 
+  
+*************** Solution 2 ******************
+Hey, Himanshu Goyal Here!
+My SID is 21105029
+I am from ECE department and my CGPA is 9.8 
+
+
+*************** Solution 3 ******************
+a...
+a & b = 8
+b...
+a | b = 58
+c...
+a ^ b = 50
+d...
+Left shifting a and b with 2 bits: a = 224 b = 40
+e...
+Right shifting a with 2 bits and b with 4 bits: a = 224 b = 160
+  
+  
+*************** Solution 4 ******************
+Enter the numbers (in " "): " 43 57 34 67 39"    
+The greatest number among the entered numbers is 67
+
+
+*************** Solution 5 ******************
+Enter desired string(in ' ') 'My name is Himanshu'
+YES
+
+Enter desired string(in ' ') 'Hello how are you'
+NO
+
+*************** Solution 6 ******************
+Enter sides of triangles(in ' ') '3 4 5'
+YES
+
+Enter sides of triangles(in ' ') '3 4 8'
+NO
