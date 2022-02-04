0c50b13f8fc85cdd1f75ebf436964b1cae593fcb
Himanshu2557 <96951932+Himanshu2557@users.noreply.github.com>
2022-02-04 23:39:19 +0530

    Update OUTPUT.py

--- Assignment-3/OUTPUT.py
+++ Assignment-3/OUTPUT.py
@@ -1,1 +1,122 @@
+------------ SOLUTION 1 ------------
+Entered desired string 'computerassignment'
+The count of:
+        'a' : 1
+        'c' : 1
+        'e' : 2
+        'g' : 1
+        'i' : 1
+        'm' : 2
+        'o' : 1
+        'n' : 2
+        'p' : 1
+        's' : 2
+        'r' : 1
+        'u' : 1
+        't' : 2
+          
+Entered desired string 'is am are computer is good python is Good'
+The count of:
+        'good' : 2
+        'python' : 1
+        'is' : 3
+        'am' : 1
+        'computer' : 1
+        'are' : 1
+         
+------------ SOLUTION 2 ------------
+Day - 28
+Month - 2
+Year - 2001
+Next Date is: 1/3/2001
+  
+Day - 28
+Month - 2
+Year - 2000
+Next Date is: 29/2/2000
+  
+------------ SOLUTION 3 ------------
+[(1, 1), (2, 4), (3, 9), (5, 25), (7, 49), (10, 100), (11, 121)]
 
+------------ SOLUTION 4 ------------
+Enter grade (between 4 and 10): 4
+Your Grade is D and Poor
+
+Enter grade (between 4 and 10): 9
+Your Grade is A and Excellent
+
+------------ SOLUTION 5 ------------
+For pattern in question enter 6
+Enter number of columns (From 0-13) 6
+  A B C D E F G H I J K 
+    A B C D E F G H I 
+      A B C D E F G 
+        A B C D E 
+          A B C 
+            A 
+           
+For pattern in question enter 6
+Enter number of columns (From 0-13) 10
+  A B C D E F G H I J K L M N O P Q R S 
+    A B C D E F G H I J K L M N O P Q 
+      A B C D E F G H I J K L M N O 
+        A B C D E F G H I J K L M 
+          A B C D E F G H I J K 
+            A B C D E F G H I 
+              A B C D E F G 
+                A B C D E 
+                  A B C 
+                    A
+                    
+------------ SOLUTION 6 ------------
+Enter name of student 'Himanshu Goyal'
+Enter SID of student 21105029
+Do you want to enter student (Y/N) 'Y'
+Enter name of student 'Kunal Singla'
+Enter SID of student 21103057
+Do you want to enter student (Y/N) 'Y'
+Enter name of student 'abc xyz'
+Enter SID of student 532617
+Do you want to enter student (Y/N) 'Y'
+Enter name of student 'My Name'
+Enter SID of student 87897216921
+Do you want to enter student (Y/N) 'N'
+-->A
+{87897216921: 'My Name', 21103057: 'Kunal Singla', 21105029: 'Himanshu Goyal', 532617: 'abc xyz'}
+-->B
+Sorted dictionary using names
+{532617: 'abc xyz', 21103057: 'Kunal Singla', 21105029: 'Himanshu Goyal', 87897216921: 'My Name'}
+-->C
+Sorted dictonary using SID's
+{87897216921: 'My Name', 532617: 'abc xyz', 21105029: 'Himanshu Goyal', 21103057: 'Kunal Singla'}
+-->D
+Enter student SID 21103057
+Name of student is Kunal Singla
+
+------------ SOLUTION 7 ------------
+Enter no. of terms till which Fibonacci sequence is to be printed: 12
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+Average Of sequence is 31.333333
+
+------------ SOLUTION 8 ------------
+-->A
+set([1, 3, 5, 6, 8])
+-->B
+set([3, 6, 8, 9, 13, 17])
+-->C
+set([1, 2, 4, 5])
+-->D
+set([8, 9, 10, 6, 7])
+-->E
+set([10, 7])
