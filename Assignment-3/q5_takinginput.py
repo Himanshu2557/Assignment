389372c4ae251091fc43c6e6e010e86c8b79730d
Himanshu2557 <96951932+Himanshu2557@users.noreply.github.com>
2022-02-04 21:21:18 +0530

    Add files via upload

--- Assignment-3/q5_takinginput.py
+++ Assignment-3/q5_takinginput.py
@@ -1,6 +1,6 @@
 print("------------ SOLUTION 4 ------------")
-
-n=int(input("Number of columns (From 0-13) ")) #Taking number of desired columns as input
+print("For pattern in question enter 6")
+n=int(input("Enter number of columns (From 0-13) ")) #Taking number of desired columns as input
 for i in range(0,n): # Loop for columns
     for j in range(0,i+1): #loop for spaces in each columns
         print(" "),
