245dc825722f6a874db36f5e3bda16098456f4f9
Himanshu2557 <himanshugoyal257@hotmail.com>
2022-02-27 19:06:03 +0530

    donee

--- Assignment_4/Q6.py
+++ Assignment_4/Q6.py
@@ -4,9 +4,9 @@ print('--------------------Question-6---------------------')
 word = input("Enter the Word: ").lower()
 
 #inputting a meaningful word with the exact same letters
-testword = input("\nEnter a MEANINGFUL word to test your friendship: ").lower()
+testword = input("\nEnter a MEANINGFULL word to test your friendship: ").lower()
 
-#defining dictionary from last assignment
+#defining dictionary to check if letters are same
 def count_in_dict(word):
     count = {}
     list1 = list(word)
@@ -19,8 +19,19 @@ def count_in_dict(word):
             count[list1[i]] = 1
     return count
 
-#test to verify the letters of the new word
+# checking same letter condition
 if count_in_dict(word) != count_in_dict(testword):
     print("The letters aren't exact, friendship is fake!!")
 else:
-    print("The word does have a meaning and letters are exact, friendship is not fake!!")
+#shopkeeper to verify the word's meaning
+    while True:
+        ans = input("\nDoes the word makes sense?(y or n)\n").lower()
+
+        if ans == 'y':
+            print("The friends pass the friendship test!!\n")
+            break
+        elif ans == 'n':
+            print("The word doesn't have a meaning, friendship is fake!!\n\n")
+            break
+        else:
+            print("Invalid input, try again")
