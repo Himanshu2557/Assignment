de80314f1785bc57482fe22e70e2cd92df306d21
Himanshu2557 <himanshugoyal257@hotmail.com>
2022-02-27 16:14:52 +0530

    donee

--- Assignment_4/Q6.py
+++ Assignment_4/Q6.py
@@ -1,1 +1,37 @@
-2
+print('--------------------Question-6---------------------')
+
+#inputting a word from the first friend
+word = input("Enter the first word: ")
+
+#inputting a meaningful word with the exact same letters
+testword = input("\nEnter a new meaningful word to test your friendship: ")
+
+#defining dictionary from last assignment
+def count_in_dict(word):
+    count = {}
+    list1 = list(word)
+    
+    n = len(list1)
+    for i in range(n):
+        if list1[i] in count:
+            count[list1[i]] += 1
+        else:
+            count[list1[i]] = 1
+    return count
+
+#test to verify the letters of the new word
+if count_in_dict(word) != count_in_dict(testword):
+    print("The letters aren't exact, friendship is fake!!")
+
+#shopkeeper's input to verify the word's meaning
+def userinput():
+    ans = input("\n{For the Shopkeeper}\nDoes the word makes sense?(y or n)\n").lower()
+
+    if ans == 'y':
+        print("The friends pass the friendship test!!\n\n")
+    elif ans == 'n':
+        print("The word doesn't have a meaning, friendship is fake!!\n\n")
+    else:
+        print("Invalid input, try again")
+        userinput()
+userinput()
--- Assignment_4/code.py
+++ Assignment_4/code.py
@@ -73,6 +73,56 @@ for i in range(0,n):  # Loop for iteration rows
         print(" %d "%(factoriali/(factorialik*factorialk)),end=" ") # print iCk ie. kth element of ith row
 print()
 
+print('--------------------Question-3---------------------')
+# Taking Input
+n1=int(input("Enter FIRST INTEGER: "))
+n2=int(input("Enter SECOND INTEGER: "))
+print()
+
+# remainder and quotient
+rem=n1%n2
+quot=int(n1/n2)
+
+print("Quotient: %d" %quot)
+print("Remainder: %d\n" %rem)
+
+# Callable value
+print("a. The quotient and reminder are a callable values.")
+print(callable(quot))
+print(callable(rem))
+
+print("b. ",end="")
+# Cheking zero value 
+if(rem==0):
+    if(quot==0):
+        print("Both values are zero")
+    else:
+        print("One value is zero")
+else:
+    if(quot==0):
+        print("One value is zero")
+
+# Set
+print("c. Filterd Values")
+reqset=set()
+for i in range (4,7):
+    newrem=rem+i
+    newquot=quot+i
+
+    if(newrem>4):
+        reqset.add(newrem)
+    if(newquot>4):
+        reqset.add(newquot)
+print(reqset)
+
+immutableset=frozenset(reqset)
+print("d. Immutable set:",immutableset)
+
+maximum=max(immutableset)
+print("e. Maximum value in set is %d" %maximum)
+print("f. Hash value: ", hash(maximum))
+
+
 print('--------------------Question-4---------------------')
 # Creating class
 class student:
