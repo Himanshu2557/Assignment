0953507f6b360eba88c218ff8b88e0b5ec9f4a65
Himanshu2557 <himanshugoyal257@hotmail.com>
2022-02-27 19:05:58 +0530

    doneee

--- Assignment_4/code.py
+++ Assignment_4/code.py
@@ -47,7 +47,6 @@ for i in range(0,n):
         print(" %d " %(nCr(i,k)),end=" ") # print iCk ie. kth element of ith row
     print(" ") # For moving to next row
 
-
 print("Using loops")
 # Taking Input and ensuring it is greater than 0
 while True:
@@ -71,7 +70,8 @@ for i in range(0,n):  # Loop for iteration rows
         for m in range(1,(i-k)+1): #finding factorial of (i-k)
             factorialik*=m
         print(" %d "%(factoriali/(factorialik*factorialk)),end=" ") # print iCk ie. kth element of ith row
-print()
+
+    print(" ")
 
 print('--------------------Question-3---------------------')
 # Taking Input
@@ -180,4 +180,42 @@ employee1.info()
 del employee3
 print("Record of Viren deleted")
 
+
+print('--------------------Question-6---------------------')
+
+#inputting a word 
+word = input("Enter the Word: ").lower()
+
+#inputting a meaningful word with the exact same letters
+testword = input("\nEnter a MEANINGFULL word to test your friendship: ").lower()
+
+#defining dictionary to check if letters are same
+def count_in_dict(word):
+    count = {}
+    list1 = list(word)
+    
+    n = len(list1)
+    for i in range(n):
+        if list1[i] in count:
+            count[list1[i]] += 1
+        else:
+            count[list1[i]] = 1
+    return count
+
+# checking same letter condition
+if count_in_dict(word) != count_in_dict(testword):
+    print("The letters aren't exact, friendship is fake!!")
+else:
+#shopkeeper to verify the word's meaning
+    while True:
+        ans = input("\nDoes the word makes sense?(y or n)\n").lower()
+
+        if ans == 'y':
+            print("The friends pass the friendship test!!\n")
+            break
+        elif ans == 'n':
+            print("The word doesn't have a meaning, friendship is fake!!\n\n")
+            break
+        else:
+            print("Invalid input, try again")
 
